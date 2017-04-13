f82bdd2630bd29a5977f20feeb7bcb8b9fb9c44c
fish-ball <57082212@qq.com>
2017-04-13 16:59:39 +0800

    每日初次运行要先创建日志文件

--- sendmaild.py
+++ sendmaild.py
@@ -32,9 +32,7 @@ if __name__ == '__main__':
 
     log_dir = os.path.join(settings.MAIL_DIR, 'errorbox')
     log_file = os.path.join(settings.MAIL_DIR, 'log', datetime.now().strftime('%Y-%m-%d')+'.log')
-    logger = open(log_file, 'a')
-    logger.save()
-    logger.close()
+    open(log_file, 'a').close()
     
 
     s.login(settings.MAIL_LOGIN, settings.MAIL_PASSWORD)
